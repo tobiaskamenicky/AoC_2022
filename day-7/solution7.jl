ccc12b95b72f7fcc138e555beceb9290dab66fed
Tobias Kamenicky <tobias.kamenicky@gmail.com>
2022-12-07 21:27:10 +0100

    Improved solution for Day7

--- day-7/solution7.jl
+++ day-7/solution7.jl
@@ -2,33 +2,27 @@ module Day07
 
 using Match
 
-function setsizes(current, edges, sizes)
-    haskey(sizes, current) && return sizes[current]
-    sizes[current] = 0
-    for n in get(edges, current, [])
-        sizes[current] += setsizes(n, edges, sizes)
-    end
-    return sizes[current]
-end
-
 function solve()
-    dirs = [""]
-    edges = Dict{String,Vector{String}}()
-    sizes = Dict{String,UInt32}()
+    dirs = [hash("")]
+    sizes = UInt32[0]
 
-    current = dirs[1]
+    current = [1]
     for line in eachline(raw"day-7/input.txt")
         if line[1] == '$'
             @match SubString(line, 3, 4) begin
                 "cd" => begin
-                    current = @match SubString(line, 6) begin
-                        "/" => ""
-                        ".." => current[1:findlast('/', current)[1]-1]
-                        _ => current * "/" * SubString(line, 6)
+                    @match SubString(line, 6) begin
+                        "/" => begin current = [1] end
+                        ".." => pop!(current)
+                        _ => begin 
+                            for d in Iterators.drop(enumerate(dirs), last(current)) 
+                                if d[2] == hash((last(current), SubString(line, 6))) 
+                                    push!(current,d[1])
+                                    break
+                                end
+                            end
+                        end
                     end
-                end
-                "ls" => begin
-                    edges[current] = String[]
                 end
             end
         else
@@ -36,34 +30,28 @@ function solve()
             name = SubString(line, si+1)
             @match SubString(line, 1, 3) begin
                 "dir" => begin
-                    path = current * "/" * name
-                    push!(dirs, path)
-                    push!(edges[current], path)
+                    push!(dirs, hash((last(current), name)))
+                    push!(sizes, 0)
                 end
-                _ => begin 
-                    path = current * "/" * name
-                    push!(edges[current], path)
-                    sizes[path] = parse(UInt32, SubString(line, 1, si-1)) 
+                _ => begin
+                    s = parse(UInt32, SubString(line, 1, si-1)) 
+                    for i in current
+                        sizes[i] += s
+                    end
                 end
             end
         end
     end
 
-    setsizes("", edges, sizes)
+    part1 = sum(Iterators.filter(<=(100000), sizes))
     
-    part1 = 0
-    for dir in dirs
-        s = sizes[dir]
-        s <= 100000 && (part1 += s)
-    end
+    needed = first(sizes) - 40000000
+    part2 = minimum(Iterators.filter(>=(needed), sizes))
 
-    needed = sizes[""] - 40000000
-    part2 = sizes[partialsort((filter(x -> sizes[x] >= needed, dirs)), 1, by= x -> sizes[x])]
-
-    return [Int(part1), Int(part2)]
+    return[part1,part2]
 end
 
 result = @time solve()
-println(result)
+println(Int.(result))
 
 end
